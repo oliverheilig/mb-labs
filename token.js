4dd70f2b35243ae51b73fbd65d8e55366c93f766
Oliver Heilig <oliver.heilig@yahoo.de>
2025-12-18 11:06:13 +0100

    Update token.js

--- token.js
+++ token.js
@@ -1,3 +1,3 @@
 // This are my personal api keys. Please get your own free key at https://developer.myptv.com/
-var token = '65BB3774-1DBC-4DF2-A775-0B36781D6290'; 
+var token = '3D26B6A8-DEC0-4640-AE58-C31C3E03C4D1'; 
 var apiKey = 'YjA2OGIzOGMxMjdiNGZkOGIwYTMwNmI0OGE4Y2ZiMTc6Y2QwZjUyMDYtMTk2Ni00N2NlLWFmNTYtNzBjOWZjZTgxNzJh';
